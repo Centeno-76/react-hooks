271863304116865ad04e7d57c01f9f0d512983c8
Centeno-76 <mateuscenteno76@gmail.com>
2021-03-24 01:27:16 +0000

    (23/03) Exercicios 1 -4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,22 +6,45 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+
+  //LAZY INIITIALIZER: incializador "preguiçoso"
+  //Quando o useState recebe uma função em vez de um valor como estado inicial,
+  //essa função é executada apenas durante a fase mount do componente, sem se repetir na fase update
+  const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
+  const [count, setCount] = React.useState(0)
+  const [nameUC, setNameUC] = React.useState(() => window.localStorage.getItem('nameUC') || initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
   function handleChange(event) {
-    setName(event.target.value)
-  }
+      setName(event.target.value)      
+    }
+    
+
+    function handleClick(event){
+        setNameUC(event.target.value.toUpperCase())
+    }
+    
+    //Efeito colateral a ser executado após o componente ter sido atualizado
+    React.useEffect(()=> {
+        //O valor do localStorage será atulalizado após a atualização do componente
+        window.localStorage.setItem('name', name)
+        window.localStorage.setItem('nameUC', nameUC)
+        setCount(count + 1)
+    }, [name, nameUC]) //[] é a lista de depêndencias, ou seja, esse useEffect é para ser chamado apenas
+    //quando a variável name sofrer alteração
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} onClick={handleClick} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        {name ? <strong>Hello {name}, {nameUC}</strong> : 'Please type your name'}
+        <p>localStorage: {window.localStorage.getItem('name')} - {window.localStorage.getItem('nameUC')}</p>
+        <p>Contagem: {count}</p>
     </div>
   )
 }
